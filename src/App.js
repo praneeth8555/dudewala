7cb5447555a39b95704c5480e51940c59f44e230
praneeth855 <mahankalipraneeth@gmail.com>
2023-07-01 13:26:39 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { CartProvider } from './components/ContextReducer';
 import './App.css';
+import Home from './screens/home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Signup from './screens/signup';
+import Orders from './screens/Orders';
+import Login from './screens/login';
+import MyOrders from './screens/myorders';
+import Deforderspage from './screens/Deforderspage';
+import Profile from './screens/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <CartProvider>
+      <Router>
+        <Routes>
+        <Route exact path="/"        element={<Home/>}/>
+        <Route exact path="/signup"  element={<Signup/>}/>
+        <Route exact path="/orders"  element={<Orders/>}/>
+        <Route exact path="/myorders"  element={<MyOrders/>}/>
+        <Route exact path="/login"   element={<Login/>}/>
+        <Route exact path="/defaultorder"   element={<Deforderspage/>}/>
+        <Route exact path="/Profile"   element={<Profile/>}/>
+        
+        </Routes>
+      </Router>
+      </CartProvider> 
+    </>
   );
 }
 
